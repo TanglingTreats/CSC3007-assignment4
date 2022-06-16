765f0ae202ad24b4ea37ddba9e91d013c7ae90e2
Edwin Tang <edwintang10@gmail.com>
2022-06-16 16:10:06 +0800

    Get data from json files.

--- script.js
+++ script.js
@@ -1,109 +1,159 @@
-const url = "";
+// Get data
+const fetch_case_data = fetch("./cases-sample.json").then((res) => {
+  return res.json();
+});
+const fetch_links_data = fetch("./links-sample.json").then((res) => {
+  return res.json();
+});
 
-let width = 1000,
-  height = 600;
+(async () => {
+  const case_data = await fetch_case_data;
+  const links_data = await fetch_links_data;
+
+  console.log(case_data);
+  console.log(links_data);
+})();
+
+let width = 800,
+  height = 800;
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("x-force");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = this.value;
+  simulation
+    .force(
+      "x",
+      d3
+        .forceX()
+        .strength(0.5)
+        .x((d) => xPosition(d.class) * this.value + width / (classNumber - 1))
+    )
+    .force(
+      "y",
+      d3
+        .forceY()
+        .strength(0.2)
+        .y(height / 2)
+    )
+    .alphaTarget(0.3)
+    .restart();
+};
 
 let svg = d3.select("svg").attr("width", width).attr("height", height);
 
-const singapore = [103.851959, 1.29027];
+let classNumber = 5;
+let colorScale = d3
+  .scaleLinear()
+  .domain([0, classNumber - 1])
+  .range([0, 1]);
 
-// Map and projection
-//let projection = d3.geoEquirectangular().center(singapore).scale(500);
-let projection = d3.geoOrthographic();
-let geopath = d3.geoPath().projection(projection);
-let graticule = d3.geoGraticule().step([10, 10]);
+let xPosition = d3
+  .scaleLinear()
+  .domain([0, classNumber - 1])
+  .range([0, 5]);
 
-// List of cities
-var cities = [
-  { name: "Singapore", longitude: 103.851959, latitude: 1.29027 },
-  { name: "London", longitude: -0.118092, latitude: 51.509865 },
-  { name: "Tokyo", longitude: 139.839478, latitude: 35.652832 },
-];
-let time = Date.now();
+let xPosStep = 110;
 
-// Load GeoJSON data
-d3.json(
-  "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"
-).then((data) => {
-  // Draw the map
-  svg
-    .append("path")
-    .datum({ type: "Sphere" })
-    .attr("id", "ocean")
-    .attr("d", geopath)
-    .attr("fill", "url(#oceanGradient)");
-  //.attr("fill", "lightBlue");
-  svg
-    .append("g")
-    .attr("id", "countries")
-    .selectAll("path")
-    .data(data.features)
-    .enter()
-    .append("path")
-    .attr("d", (d) => geopath(d))
-    .attr("fill", "#777")
-    .attr("stroke", "#fff")
-    .attr("stroke-width", 0.5)
-    .on("mouseover", (event, d) => {
-      d3.select(".tooltip")
-        .text(d.properties.name)
-        .style("position", "absolute")
-        .style("background", "#fff")
-        .style("left", event.pageX + "px")
-        .style("top", event.pageY + "px");
-      d3.select(event.target).attr("class", "country").classed("select", true);
-    })
-    .on("mouseout", (event, d) => {
-      d3.select(".tooltip").text("");
-      d3.select(event.target).attr("class", "country").classed("select", false);
-    });
-  svg
-    .append("g")
-    .attr("id", "graticules")
-    .selectAll("path")
-    .data([graticule()])
-    .enter()
-    .append("path")
-    .attr("d", (d) => geopath(d))
-    .attr("fill", "none")
-    .attr("stroke", "#aaa")
-    .attr("stroke-width", 0.2);
-  svg
-    .append("g")
-    .attr("id", "cities")
-    .selectAll("circle")
-    .data(cities)
-    .enter()
-    .append("circle")
-    .attr("cx", (d) => projection([d.longitude, d.latitude])[0])
-    .attr("cy", (d) => projection([d.longitude, d.latitude])[1])
-    .attr("r", 5)
-    .attr("fill", "yellow");
-  let time = Date.now();
+let numOfData = 100;
+let data = [];
+for (let i = 0; i < numOfData; i++) {
+  let obj = { x: 400, y: 400 };
+  obj.id = "node" + i;
+  obj.class = Math.floor(Math.random() * classNumber);
+  data.push(obj);
+}
 
-  d3.timer(function () {
-    let angle = (Date.now() - time) * 0.02;
-    projection.rotate([angle, 0, 0]);
-    svg
-      .select("g#countries")
-      .selectAll("path")
-      .attr("d", geopath.projection(projection));
-    svg
-      .select("g#graticules")
-      .selectAll("path")
-      .attr("d", geopath.projection(projection));
-    svg
-      .select("g#cities")
-      .selectAll("circle")
-      .attr("cx", (d) => projection([d.longitude, d.latitude])[0])
-      .attr("cy", (d) => projection([d.longitude, d.latitude])[1])
-      .attr("visibility", (d) => {
-        var point = { type: "Point", coordinates: [d.longitude, d.latitude] };
-        if (geopath(point) == null) {
-          return "hidden";
-        } else {
-          return "visible";
-        }
-      });
+let links = [];
+for (let i = 0; i < numOfData / 2; i++) {
+  let obj = {};
+  obj.source = "node" + Math.floor((Math.random() * numOfData) / 2);
+  obj.target = "node" + Math.floor(Math.random() * numOfData);
+  links.push(obj);
+}
+
+let node = svg
+  .append("g")
+  .attr("id", "nodes")
+  .selectAll("circle")
+  .data(data)
+  .enter()
+  .append("circle")
+  .attr("r", 10)
+  .style("fill", (d) => d3.interpolateViridis(colorScale(d.class)))
+  .call(
+    d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended)
+  );
+
+let linkpath = svg
+  .append("g")
+  .attr("id", "links")
+  .selectAll("path")
+  .data(links)
+  .enter()
+  .append("path")
+  .attr("fill", "none")
+  .attr("stroke", "black");
+
+let simulation = d3
+  .forceSimulation()
+  .nodes(data)
+  .force(
+    "x",
+    d3
+      .forceX()
+      .strength(0.5)
+      .x((d) => xPosition(d.class) * xPosStep + width / (classNumber - 1))
+  )
+  .force(
+    "y",
+    d3
+      .forceY()
+      .strength(0.3)
+      .y(height / 2)
+  )
+  .force("charge", d3.forceManyBody().strength(0))
+  .force("collide", d3.forceCollide().strength(0.1).radius(15))
+  .force(
+    "link",
+    d3
+      .forceLink(links)
+      .id((d) => d.id)
+      .distance(60)
+      .strength(0.5)
+  )
+  .on("tick", (d) => {
+    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    linkpath.attr(
+      "d",
+      (d) =>
+        "M" +
+        d.source.x +
+        "," +
+        d.source.y +
+        " " +
+        d.target.x +
+        "," +
+        d.target.y
+    );
   });
-});
+
+function dragstarted(event, d) {
+  if (!event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(event, d) {
+  d.fx = event.x;
+  d.fy = event.y;
+}
+
+function dragended(event, d) {
+  if (!event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
